193610d93f4c9355fc85756325cdb5b2cb5f568d
Daksh <dakshgondi06@gmail.com>
2025-11-17 16:49:18 +0530

    uploaded components for front end

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import gradient from '../assets/gradient.png'
+import star2 from '../assets/star2.png'
+const Header = () => {
+return (
+    <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
+        {/* Gradient background covering the entire screen */}
+        <img
+            src={gradient}
+            alt="header"
+            className="fixed top-0 left-0 w-screen h-screen object-cover -z-10 opacity-80 pointer-events-none"
+            style={{ minHeight: '100vh' }}
+        />
+        <div className='text-center mt-20 mb-8 z-10'>
+            <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary'>
+                <p>Ai Feature Integrated</p>
+                <img src={star2} className='w-2.5' alt='Star icon'/>
+            </div>
+
+            <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700'> <span className='text-primary'>Blogging</span> On The <br/> GO!</h1>
+            <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>This is your space to think out loud, to share what matters, 
+                and to write without filters. Whether it's one word or a thousand, your story starts right here.</p>
+
+            <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+                <input type="text" placeholder='Search for Blogs' required className='w-full pl-4 outline-none'/>
+                <button type="Submit" className='bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
+            </form>
+
+        </div>
+    </div>
+)
+}
+
+export default Header
+
+        
